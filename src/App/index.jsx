08c346b16550f092a83e762bb35fd2ebe7b6485b
vamci07 <vamci07@gmail.com>
2021-04-19 14:48:18 -0400

    add support for images, avg

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,4 +1,6 @@
 import Recipes from '../components/Recipes';
+import sword from '../assets/images/swc-sword.png';
+import swordSvg from '../assets/icons/sword.svg';
 
 export default function App() {
   return (
@@ -8,8 +10,10 @@ export default function App() {
         <section>
           <h1>Oh Hai, React!</h1>
         </section>
+        <img src={sword} alt='sword' width='250' />
+        <img src={swordSvg} alt='sword' width='250' />
+        <Recipes />
       </main>
-      <Recipes />
     </>
   );
 }
